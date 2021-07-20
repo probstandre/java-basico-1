b0ea0ac19f50129a10d30fa9654d96cd514e5a39
Edward <edward.neto@triersistemas.com.br>
2021-07-20 11:11:29 -0300

    resultado

--- Programa.java
+++ Programa.java
@@ -7,28 +7,46 @@ public class Programa {
     // [[ijij][rdrdrdr]]
     public static void main(String[] args_) {
 
-        String[] args = { "add", "20", "1" };
+        String[] args = { "add", "feijao", "hamburger", "frango", "cafe" };
         System.out.println("Iniciado");
         if (Objects.nonNull(args) && args.length > 0) {
             
             if ("soma".equals(args[0])) {
                 soma(args);
             } else if ("add".equals(args[0])) {
-                List<String> compras = new ArrayList<>();
-                Integer i = 0;
-                while (i < args.length) {
-                    compras.add(args[i]);
-                    ++i; // explicar 
-                }
-                System.out.println(Arrays.toString(compras.toArray()));
+                add(args);
+            } else if ("exp".equals(args[0])) {
+                exp();
             } else {
-                for (String item : args) { // trasforme para metodo
-                    System.out.println("voce informou: " + item);
-                }
+                listar(args);
             }
         } else {
             System.out.println("Em args tem menos que 1");
         }
+    }
+
+    private static void exp() {
+        Integer a = 0;
+        Integer b = 0;
+        System.out.println(a++);
+        System.out.println(++b);
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+    }
+
+    private static void listar(String[] args) {
+        for (String item : args) { // trasforme para metodo
+            System.out.println("voce informou: " + item);
+        }
+    }
+
+    private static void add(String[] args) {
+        List<String> compras = new ArrayList<>();
+        Integer i = 0;
+        while (++i < args.length) {
+            compras.add(args[i]);
+        }
+        System.out.println(Arrays.toString(compras.toArray()));
     }
 
     private static void soma(String[] args) {
