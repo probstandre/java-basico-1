37a2e1f808e2e9abbaf987013d02e337dc0bc5b6
Edward <edward.neto@triersistemas.com.br>
2021-07-20 10:19:14 -0300

    tarefas

--- Programa.java
+++ Programa.java
@@ -1,21 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
 
 public class Programa {
     // [[ijij][rdrdrdr]]
     public static void main(String[] args_) {
 
-        String[] args = { "somax", "20", "1" };
+        String[] args = { "add", "20", "1" };
         System.out.println("Iniciado");
         if (Objects.nonNull(args) && args.length > 0) {
             
             if ("soma".equals(args[0])) {
-                Integer soma = 0;
-                for (int i = 1; i < args.length; i++) { // i = i + 1
-                    soma += Integer.valueOf(args[i]);
+                soma(args);
+            } else if ("add".equals(args[0])) {
+                List<String> compras = new ArrayList<>();
+                Integer i = 0;
+                while (i < args.length) {
+                    compras.add(args[i]);
+                    ++i; // explicar 
                 }
-                System.out.println("O resultado da soma e: " + soma);
+                System.out.println(Arrays.toString(compras.toArray()));
             } else {
-                for (String item : args) {
+                for (String item : args) { // trasforme para metodo
                     System.out.println("voce informou: " + item);
                 }
             }
@@ -23,12 +30,12 @@ public class Programa {
             System.out.println("Em args tem menos que 1");
         }
     }
-    /*
-    Informe qualquer valor​
 
-    Soma 1 2 5 8 9​
-
-    Multiplica 4 8 6 7
-    */
-
+    private static void soma(String[] args) {
+        Integer soma = 0;
+        for (int i = 1; i < args.length; i++) { // i = i + 1
+            soma += Integer.valueOf(args[i]);
+        }
+        System.out.println("O resultado da soma e: " + soma);
+    }
 }
