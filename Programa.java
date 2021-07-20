2fe272dd3135e8635d2fc66658adc8e72ca013b7
Edward <edward.neto@triersistemas.com.br>
2021-07-20 16:23:16 -0300

    correcao

--- Programa.java
+++ Programa.java
@@ -6,23 +6,15 @@ import java.util.Objects;
 public class Programa {
     // [[ytftyfty][tftft][ygygy]]
     public static void main(String[] args_) {
-        String[] args = { "add", "2", "8" };
+        String[] args = { "add", "monitor", "1kg de farinha", "batata" };
         System.out.println("Ligou");
         if (Objects.nonNull(args) && args.length > 0) {
             if ("soma".equals(args[0])) {
-                Integer soma = 0;
-                for (int i = 1; i < args.length; i++) { // i = i + 1 
-                    soma += Integer.valueOf(args[i]);
-                }
-                System.out.println("A soma dos valores: " + soma);
+                soma(args);
             } else if ("add".equals(args[0])) {
-                List<String> compras = new ArrayList<>();
-                Integer i = 0;
-                while (i < args.length) {
-                    compras.add(args[i]);
-                    ++i;
-                }
-                System.out.println(Arrays.toString(compras.toArray()));
+                add(args);
+            } else if ("exp".equals(args[0])) {
+                exp();
             } else {
                 listar(args);
             }
@@ -31,9 +23,35 @@ public class Programa {
         }
     }
 
+    private static void add(String[] args) {
+        List<String> compras = new ArrayList<>();
+        Integer i = 0;
+        while (++i < args.length) {
+            compras.add(args[i]);
+        }
+        System.out.println(Arrays.toString(compras.toArray()));
+    }
+
+    private static void soma(String[] args) {
+        Integer soma = 0;
+        for (int i = 1; i < args.length; i++) { // i = i + 1 
+            soma += Integer.valueOf(args[i]);
+        }
+        System.out.println("A soma dos valores: " + soma);
+    }
+
     private static void listar(String[] args) {
         for (String item : args) {
             System.out.println(item);
         }
+    }
+
+    private static void exp() {
+        Integer a = 0;
+        Integer b = 0;
+        System.out.println(a++);
+        System.out.println(++b);
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
     }
 }
