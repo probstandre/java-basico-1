a6cd59c95557d056f6eac3ba9a0ac4dc024c2d71
Edward <edward.neto@triersistemas.com.br>
2021-07-21 13:07:34 -0300

    projeto

--- ListaCompra.java
+++ ListaCompra.java
@@ -1,5 +1,3 @@
-package br.com.edward.trijava;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
--- Programa.java
+++ Programa.java
@@ -1,5 +1,3 @@
-package br.com.edward.trijava;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
